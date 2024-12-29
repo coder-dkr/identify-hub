f1871cc23fe915e118533aa23a105a71f8d4c6be
Dhruv Roy <dhruvakr9001@gmail.com>
2024-12-29 15:27:47 +0530

    crafted

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,9 @@ const App = () => {
       <Future/>
       <Footer/>
       <Analytics/>
+      <div className="fixed bottom-3 left-1/2 -translate-x-[50%] text-center rounded-full bg-white text-black shadow-lg px-4 py-2 font-semibold">
+        Crafted with ✋ by <a className="font-bold" target="_blank" href="https://dhruvroy.in">Dhruv Roy</a>
+      </div>
     </>
   )
 }
